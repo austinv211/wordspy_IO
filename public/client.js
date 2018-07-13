cce112868da4f1cb8fabf4faea756c357c7797db
austin vargason <vargasona@gmail.com>
2018-07-12 23:07:00 -0700

    added node files and p5.js

--- public/client.js
+++ public/client.js
@@ -1,32 +1,41 @@
+//Client variable
 var Client = {};
 
+//set the socket to io.connect 
 Client.socket = io.connect();
 
+//set the player name to the value from local storage
 Client.playerName = window.localStorage.getItem("name");
 
+//function on what to do on connect function
 Client.socket.on('connect', function() {
     console.log("my id " + Client.socket.id);
 });
 
+//get the room name from the location
 Client.room = window.location.hash.substring(1);
 
-
+//variable for team name
 Client.team = null;
 
+//fucntion to set the team
 Client.setTeam = function(team){
     console.log(Client.playerName);
     Client.team = team;
     console.log("my team name is " + Client.team);
 };
 
+//function get players
 Client.getPlayers = function (){
     Client.socket.emit('getPlayers', Client.room);
 };
 
+//function to ask for a new player
 Client.askNewPlayer = function(name) {
     Client.socket.emit('newPlayer', Client.playerName, Client.room);
 };
 
+//invoke functions
 Client.askNewPlayer();
 Client.getPlayers();
 
--- public/getroom.js
+++ public/getroom.js
@@ -1,7 +1,12 @@
+//declare variable room
 var room;
 
+//function to submit the room name
 function submitRoom(roomName) {
+    //get the room field
     room = document.getElementById("rmFld").value;
+    //save the value of the name field to local storage
     window.localStorage.setItem("name", document.getElementById("nameField").value);
+    //append the url with the room name
     window.location.href = 'game.html' + "#" + room;
 }
